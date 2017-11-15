7c8a4aa487dc671e3d537bae12f7e74fd50e71aa
Luis Lopez <delucholuis@gmail.com>
2017-11-15 14:39:19 -0800

    NamePrompt

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpFoundations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type Name Below");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}!");
+        }
+    }
+}
