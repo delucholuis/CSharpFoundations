f4a47c68b5cddd23f1ed81dbbbf6c2fba3d52890
Luis Lopez <delucholuis@gmail.com>
2017-11-17 11:26:44 -0800

    NameImput#3

--- Program.cs
+++ Program.cs
@@ -6,13 +6,11 @@ namespace CSharpFoundations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Value Between 5 And 20 Below");
-            int myResponse = int.Parse (Console.ReadLine());
-
-            int i=1;
-            while (i <= myResponse){
-                Console.WriteLine(i + (i % 2 == 0 ? " is even" :  " is odd"));
-                i++;
+            Console.WriteLine("Type Name Below");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}!");{
+                Console.WriteLine("Push ENTER to End Program");
+                Console.ReadLine();
             }
         }
     }
